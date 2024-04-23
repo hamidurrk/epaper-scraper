5a70f92cf03bdc4e445047ee20aa87645d52ba79
hamidurrk <hamidurrk@gmail.com>
2024-04-23 06:00:09 +0600

    frame laid

--- src/main.py
+++ src/main.py
@@ -83,6 +83,14 @@ class App(customtkinter.CTk):
         image_path = os.path.join(BASE_DIR, "resources")
         self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
         self.large_test_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "large_test_image.png")), size=(500, 150))
+        
+        self.jugantor_img = customtkinter.CTkImage(Image.open(os.path.join(image_path, "jugantor.jpg")), size=(130, 35))
+        self.jugantor_img_lg = customtkinter.CTkImage(Image.open(os.path.join(image_path, "jugantor.jpg")), size=(500, 150))
+        self.prothomalo_img = customtkinter.CTkImage(Image.open(os.path.join(image_path, "prothom_alo.png")), size=(130, 35))
+        self.prothomalo_img_lg = customtkinter.CTkImage(Image.open(os.path.join(image_path, "prothom_alo.png")), size=(500, 150))
+        self.kalkerkantho_img = customtkinter.CTkImage(Image.open(os.path.join(image_path, "kaler_kantho.png")), size=(130, 35))
+        self.kalkerkantho_img_lg = customtkinter.CTkImage(Image.open(os.path.join(image_path, "kaler_kantho.png")), size=(500, 150))
+        
         self.image_icon_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "image_icon_light.png")), size=(20, 20))
         self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
                                                  dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
@@ -101,19 +109,19 @@ class App(customtkinter.CTk):
         self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
         
         # jugantor
-        self.jugantor_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Home",
+        self.jugantor_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="",
                                                    fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                   image=self.home_image, anchor="w", command=self.jugantor_button_event)
+                                                   image=self.jugantor_img, anchor="w", command=self.jugantor_button_event)
         self.jugantor_button.grid(row=1, column=0, sticky="ew")
 
-        self.prothomalo_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 2",
+        self.prothomalo_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.chat_image, anchor="w", command=self.prothomalo_button_event)
+                                                      image=self.prothomalo_img, anchor="w", command=self.prothomalo_button_event)
         self.prothomalo_button.grid(row=2, column=0, sticky="ew")
 
-        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Frame 3",
+        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="",
                                                       fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
-                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+                                                      image=self.kalkerkantho_img, anchor="w", command=self.frame_3_button_event)
         self.frame_3_button.grid(row=3, column=0, sticky="ew")
 
         self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
@@ -124,23 +132,31 @@ class App(customtkinter.CTk):
         self.jugantor_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
         self.jugantor_frame.grid_columnconfigure(0, weight=1)
 
-        self.jugantor_frame_large_image_label = customtkinter.CTkLabel(self.jugantor_frame, text="", image=self.large_test_image)
-        self.jugantor_frame_large_image_label.grid(row=0, column=0, padx=20, pady=10)
+        self.jugantor_frame_large_image_label = customtkinter.CTkLabel(self.jugantor_frame, text="", image=self.jugantor_img_lg)
+        self.jugantor_frame_large_image_label.grid(row=0, column=0, padx=20, pady=50)
 
-        self.jugantor_frame_button_1 = customtkinter.CTkButton(self.jugantor_frame, text="", image=self.image_icon_image)
-        self.jugantor_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
-        self.jugantor_frame_button_2 = customtkinter.CTkButton(self.jugantor_frame, text="CTkButton", image=self.image_icon_image, compound="right")
-        self.jugantor_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
-        self.jugantor_frame_button_3 = customtkinter.CTkButton(self.jugantor_frame, text="CTkButton", image=self.image_icon_image, compound="top")
-        self.jugantor_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
-        self.jugantor_frame_button_4 = customtkinter.CTkButton(self.jugantor_frame, text="CTkButton", image=self.image_icon_image, compound="bottom", anchor="w")
-        self.jugantor_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
+        # self.jugantor_frame_button_1 = customtkinter.CTkButton(self.jugantor_frame, text="", image=self.image_icon_image)
+        # self.jugantor_frame_button_1.grid(row=1, column=0, padx=20, pady=10)
+        # self.jugantor_frame_button_2 = customtkinter.CTkButton(self.jugantor_frame, text="CTkButton", image=self.image_icon_image, compound="right")
+        # self.jugantor_frame_button_2.grid(row=2, column=0, padx=20, pady=10)
+        # self.jugantor_frame_button_3 = customtkinter.CTkButton(self.jugantor_frame, text="CTkButton", image=self.image_icon_image, compound="top")
+        # self.jugantor_frame_button_3.grid(row=3, column=0, padx=20, pady=10)
+        # self.jugantor_frame_button_4 = customtkinter.CTkButton(self.jugantor_frame, text="CTkButton", image=self.image_icon_image, compound="bottom", anchor="w")
+        # self.jugantor_frame_button_4.grid(row=4, column=0, padx=20, pady=10)
 
         # create second frame
         self.prothomalo_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.prothomalo_frame.grid_columnconfigure(0, weight=1)
+
+        self.prothomalo_frame_large_image_label = customtkinter.CTkLabel(self.prothomalo_frame, text="", image=self.prothomalo_img_lg)
+        self.prothomalo_frame_large_image_label.grid(row=0, column=0, padx=20, pady=50)
 
         # create third frame
         self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+        self.third_frame.grid_columnconfigure(0, weight=1)
+
+        self.third_frame_large_image_label = customtkinter.CTkLabel(self.third_frame, text="", image=self.kalkerkantho_img_lg)
+        self.third_frame_large_image_label.grid(row=0, column=0, padx=20, pady=50)
 
         # select default frame
         self.select_frame_by_name("jugantor")
